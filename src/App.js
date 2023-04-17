ec5f2410d0842bed1b595fc2c48678b11a5d60d3
Noman Shaikh <noman.shaikh@neosoftmail.com>
2023-04-17 15:33:36 +0530

    working on Sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./components/home/Home";
+import { Sidebar } from "./components/sidebar/Sidebar";
 import Header from "./components/header/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="app">
       <Router>
+        <Header />
         <>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
+          <AppBody>
+            <Routes>
+              <Route path="/" element={<Sidebar />} />
+            </Routes>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -20,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div``;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Sidebar = () => {
+  return <div>{/* <h1>Sidebar</h1> */}</div>;
+};
